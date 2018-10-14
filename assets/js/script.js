5e1e7f6b0df9829b648ae3794c837b923925b862
Thomas Dethorey <thomas.dethorey01@gmail.com>
2018-10-14 16:10:26 +0200

    ex 3

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function carre1(){
+  document.getElementById("text").style.color = "green";
+}
+function carre2(){
+  document.getElementById("text").style.color = "red";
+}
+function carre3(){
+  document.getElementById("text").style.color = "blue";
+}
